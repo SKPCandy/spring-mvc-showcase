bea3285ec1a71829c47346a0cbdec467bba67708
horanghi <horanghi@naver.com>
2013-10-14 15:01:16 +0900

    revert

--- src/main/java/org/springframework/samples/mvc/simple/SimpleController.java
+++ src/main/java/org/springframework/samples/mvc/simple/SimpleController.java
@@ -1,32 +1,15 @@
 package org.springframework.samples.mvc.simple;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.skplanet.plandasj.Plandasj;
-import com.skplanet.plandasj.PlandasjPool;
-
 @Controller
 public class SimpleController {
 
-	@Autowired
-	PlandasjPool pPool;
-
-	final String key = "hello";
-	final String value = "Plandas's world";
-
 	@RequestMapping("/simple")
-	public @ResponseBody
-	String simple() {
-		Plandasj client = pPool.getClient();
-		client.set(key, value);
-
-		String retValue = client.get(key);
-
-		client.del(key);
-		return retValue;
+	public @ResponseBody String simple() {
+		return "Hello world!";
 	}
 
 }
